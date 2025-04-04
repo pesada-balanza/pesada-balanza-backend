efdbfdb74e4b11b986e5d119d0c96ec61acc327d
Matias Moschen <matias@amh.com.ar>
2025-04-04 09:03:38 -0300

    Fix console log message in app.js

--- app.js
+++ app.js
@@ -193,5 +193,5 @@ app.put('/anular/:id', requireCode('1234', '/'), async (req, res) => {
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
-    console.log('Servidor corriendo en http://0.0.0.0:${PORT}');
+    console.log("Servidor corriendo en http://0.0.0.0:${PORT}");
 });
